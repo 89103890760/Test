c5b07d99469e5eb01752aeda250b9e17566aebff
89103890760 <92647252+89103890760@users.noreply.github.com>
2022-07-25 19:20:32 +0300

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Data.Entity;
+using System.Data;
+
+
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Данные_компанииEntities1 comp = new Данные_компанииEntities1();
+            
+        }
+    }
+}
